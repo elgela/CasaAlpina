df2b7851b5b60adfce5b66af70389cdb72e82f6f
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-11-12 02:15:05 -0300

    different hamburger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -490,3 +490,20 @@ async function createAssessment({
     return null;
   }
 }
+
+////////////////////////barra menu/////////////////////////////////////////
+
+document.querySelector(".bars__menu").addEventListener("click", animateBars);
+
+let line1__bars = document.querySelector(".line1__bars-menu");
+let line2__bars = document.querySelector(".line2__bars-menu");
+let line3__bars = document.querySelector(".line3__bars-menu");
+let container__menu = document.querySelector(".nav-list");
+
+function animateBars() {
+    line1__bars.classList.toggle("activeline1__bars-menu");
+    line2__bars.classList.toggle("activeline2__bars-menu");
+    line3__bars.classList.toggle("activeline3__bars-menu");
+
+    container__menu.classList.toggle("nav-list__active");
+}
