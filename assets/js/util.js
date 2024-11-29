55f99c0ab1398472c2a5e5666a6209442bf93cec
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-11-29 10:39:51 -0300

    line4__bars-menu added

--- assets/js/util.js
+++ assets/js/util.js
@@ -593,12 +593,14 @@ document.querySelector(".bars__menu").addEventListener("click", animateBars);
 var line1__bars = document.querySelector(".line1__bars-menu");
 var line2__bars = document.querySelector(".line2__bars-menu");
 var line3__bars = document.querySelector(".line3__bars-menu");
+var line4__bars = document.querySelector(".line4__bars-menu");
 var container__menu = document.querySelector(".nav-list");
 
 function animateBars() {
     line1__bars.classList.toggle("activeline1__bars-menu");
     line2__bars.classList.toggle("activeline2__bars-menu");
     line3__bars.classList.toggle("activeline3__bars-menu");
+    line4__bars.classList.toggle("activeline4__bars-menu");
 
     container__menu.classList.toggle("nav-list__active");
 }
