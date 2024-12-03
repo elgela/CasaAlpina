841715a3cc16bf8a2e173d5347d01ef10667f0c1
Marcelo Gelato <gelatomarcelo77@gmail.com>
2024-12-02 23:03:58 -0300

    jquery irArriba added

--- assets/js/util.js
+++ assets/js/util.js
@@ -605,3 +605,14 @@ function animateBars() {
     container__menu.classList.toggle("nav-list__active");
 }
 
+////////////////////ir arriba////////////////////////////////////////
+
+$(document).ready(function(){ irArriba(); }); //Hacia arriba
+
+function irArriba(){
+  $('.ir-arriba').click(function(){ $('body,html').animate({ scrollTop:'0px' },1000); });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){ $('.ir-arriba').slideDown(600); }else{ $('.ir-arriba').slideUp(600); }
+  });
+  $('.ir-abajo').click(function(){ $('body,html').animate({ scrollTop:'1000px' },1000); });
+}
