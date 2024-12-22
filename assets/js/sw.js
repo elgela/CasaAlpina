d97f8a686e4864b8c0790496075b6ff51871bc1f
Marcelo Gelato <gelatomarcelo@hotmail.com>
2024-12-22 20:58:29 -0300

    registro Service Work

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,14 +1,14 @@
 //////////para instalar como aplicacion (no está funcionando ahora). Registro de Service Worker//////////////
 
-// self.addEventListener('install', function(event) {
-//     console.log('Service Worker instalado');
-//   });
+self.addEventListener('install', function(event) {
+    console.log('Service Worker instalado');
+  });
   
-//   self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//       caches.match(event.request).then(function(response) {
-//         return response || fetch(event.request);
-//       })
-//     );
-//   });
-  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
+    
